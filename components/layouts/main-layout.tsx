db28365422736bffadf8922eef09fe34efba8549
闫冬 <yandong@smartisan.com>
2024-02-13 11:59:33 +0800

    google cosole

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -23,6 +23,7 @@ export default function MainLayout(props: MainLayoutProps) {
           name="viewport"
           content="width=device-width, initial-scale=1"
         />
+        <meta name="google-site-verification" content="OmaMgn0nn-RX9l_KyfAHFFXEPhgzedPD9ZCaB21EyxA" />
         <link
           rel="icon"
           href="/favicon.ico"
@@ -38,7 +39,8 @@ export default function MainLayout(props: MainLayoutProps) {
       >
         {props.children}
       </Flex>
-      <Faqs/>
+      <Faqs />
+      
       <Footer />
     </>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/poppins/400.css";
+import "../styles/tailwind.css";
 
 import theme from "../theme";
 
